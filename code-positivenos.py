06fa20cf7a061eeae023bbdff213722b240e18cc
sakshighag <87032028+sakshighag@users.noreply.github.com>
2021-07-13 13:37:24 +0530

    Add files via upload

---
+++ code-positivenos.py
@@ -0,0 +1,27 @@
+# Python program to print positive Numbers in a List
+
+# list of numbers
+list1 = [12, -7, 5, 64, -14]
+
+
+# iterating each number in list
+for num in list1:
+	
+	# checking condition
+	if num >= 0:
+    print "Positive numbers list: 12,5,64
+
+# Python program to print positive Numbers in a List
+
+# list of numbers
+list2 = [12, 14, -95, 3]
+
+
+# iterating each number in list
+for num in list2:
+	
+	# checking condition
+	if num >= 0:
+    print "Positive numbers list: 12,14,3
+
+
